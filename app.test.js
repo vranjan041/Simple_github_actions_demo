f48de421660ee11511efd47a284fa711656594a7
Vivek Ranjan <vranjan041@gmail.com>
2024-09-24 06:43:19 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,18 +1,18 @@
-const request = require('supertest');
-const app = require('./index.js'); // Import the app
+import request from "supertest";
+import app from "./index.js"; // Import the app
 
 describe('Test the root path', () => {
-  it('should respond with "Hello World"', async () => {
+  it('should respond with "Home page"', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello World');
+    expect(res.text).toBe('Home page');
   });
 });
 
 describe('Test the /about path', () => {
-  it('should respond with "About Page"', async () => {
+  it('should respond with "About page"', async () => {
     const res = await request(app).get('/about');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('About Page');
+    expect(res.text).toBe('About page');
   });
 });
