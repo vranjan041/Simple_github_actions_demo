10d20971c8c6454ef23be0297d3b28f889e505be
Vivek Ranjan <vranjan041@gmail.com>
2024-09-24 06:51:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ app.get("/about", (req, res) => {
   res.send("About page");
 });
 
+if (require.main === module) {
 app.listen(3000, () => {
   console.log("Server is running on port 3000...");
-});
+})};
 
 module.exports = app
