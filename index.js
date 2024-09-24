d47ca54c5f1c1ff7a7e1b7a03909090e1144d79b
Vivek Ranjan <vranjan041@gmail.com>
2024-09-24 06:43:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express');
-app=express();
-app.get('/',(req,res)=>{
- res.send('Hello World');   
-})
-app.get('/about',(req,res)=>{
-res.send('About Page');    
-})
-if (require.main === module) {
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000');
-    });
-  }
-module.exports = app
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000...");
+});
+
+export default app
